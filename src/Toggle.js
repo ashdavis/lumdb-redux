ab7bc7117d2d3a929179113c6b49578bfe46dfcb
Ashley Davis <ash.webaccounts@me.com>
2018-03-19 12:40:04 +0000

    Added an action creator to actions.js and used that action in Toggle
    - Created actions.js to store action creators (functions that return action objects)
    - Added toggleMessage action created to actions.js
    - Imported toggleMessage in Toggle.js
    - Passed toggleMessage as an argument to dispatch, calling the function to get the action object

--- src/Toggle.js
+++ src/Toggle.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import { toggleMessage } from './actions';
+
 const Toggle = ({messageVisible, dispatch}) => (
   <div>
     {messageVisible &&
       <p>You will see this if the Redux Action is toggled</p>
     }
-    <button onClick={() => dispatch({type: 'TOGGLE_MESSAGE'})}>
+    <button onClick={() => dispatch(toggleMessage())}>
       Toggle Me
     </button>
   </div>
