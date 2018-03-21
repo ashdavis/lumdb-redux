b9407932ad43c544ea05c8c09545a9151c592ed9
Ashley Davis <ash.webaccounts@me.com>
2018-03-21 19:08:02 +0000

    Updated MovieDetail to connect to Redux and dispatch actions
    - Added movie and movieLoaded properties to initialState in movies reducer
    - Created getMovie and resetMovie actions and action type consts in src/movies/actions.js
    - getMovie is an action thunk that accepts an id, fetches movie data and dispatches GET_MOVIE action
    - resetMovie simply dispatches RESET_MOVIE action
    - Added GET_MOVIE and RESET_MOVIE cases to movies reducer
    - GET_MOVIE case returns new state object with movie set as action data and movieLoaded set to true
    - RESET_MOVIE case returns a new state object with movie set to an empty object and movieLoaded set to false
    - Imported connect and bindActionCreators in MovieDetail
    - Imported getMovie and resetMovie action creators in MovieDetail
    - Added mapStateToProps and mapDispatchToProps functions to MovieDetail
    - Added connect to MovieDetail export statement
    - Simplified conditional logic in MovieDetail render method
    - Replaced API call in MovieDetail componentDidMount method with call to getMovie
    - Added componentWillUnmount method with call to resetMovie to reset movie state on unmount

--- src/movies/MovieDetail.js
+++ src/movies/MovieDetail.js
@@ -1,61 +1,57 @@
 import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import styled from 'styled-components';
 import Overdrive from 'react-overdrive';
+
 import { Poster } from './Movie';
-import API_ROOT from '../api-config';
+import { getMovie, resetMovie } from './actions';
 
 const POSTER_PATH = 'http://image.tmdb.org/t/p/w342';
 const BACKDROP_PATH = 'http://image.tmdb.org/t/p/w1280';
 
 class MovieDetail extends PureComponent {
-  state = {
-    movie: null
+  componentDidMount() {
+    const {match, getMovie} = this.props;
+    getMovie(match.params.id);
   }
 
-  async componentDidMount() {
-    const {match} = this.props;
-    // const url = `https://api.themoviedb.org/3/movie/${match.params.id}?api_key=b73ab608261783389bdc277a5b6bf166&language=en-US`;
-    const url = `${API_ROOT}/movie/${match.params.id}`;
-    try {
-      const response = await fetch(url);
-      const movie = await response.json();
-      this.setState({
-        movie
-      });
-    } catch (error) {
-      console.log(error);
-    }
+  componentWillUnmount() {
+    const {resetMovie} = this.props;
+    resetMovie();
   }
 
   render() {
-    const {movie} = this.state;
-    if (movie) {
-      return (
-        <MovieWrapper backdrop={`${BACKDROP_PATH}${movie.backdrop_path}`}>
-          <MovieInfo>
-            <Overdrive id={movie.id.toString()}>
-              <Poster src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title}/>
-            </Overdrive>
-              <div>
-                {movie.title === 'It' ? (
-                  <h1>It</h1>
-                ) : (
-                  <h1>{movie.title}</h1>
-                )}
-                {/* <h1>{movie.title}</h1> */}
-                <h3>{movie.release_date}</h3>
-                <p>{movie.overview}</p>
-              </div>
-          </MovieInfo>
-        </MovieWrapper>
-      );
-    } else {
-      return null;
-    }
+    const {movie} = this.props;
+    if (!movie.id) return null;
+    return (
+      <MovieWrapper backdrop={`${BACKDROP_PATH}${movie.backdrop_path}`}>
+        <MovieInfo>
+          <Overdrive id={movie.id.toString()}>
+            <Poster src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title}/>
+          </Overdrive>
+            <div>
+              <h1>{movie.title}</h1>
+              <h3>{movie.release_date}</h3>
+              <p>{movie.overview}</p>
+            </div>
+        </MovieInfo>
+      </MovieWrapper>
+    );
   }
 }
 
-export default MovieDetail;
+const mapStateToProps = ({movies}) => ({
+  movie: movies.movie,
+  loaded: movies.movieLoaded,
+});
+
+const mapDispatchToProps = (dispatch) => bindActionCreators({
+  getMovie,
+  resetMovie,
+}, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(MovieDetail);
 
 const MovieWrapper = styled.div`
   position: relative;
--- src/movies/actions.js
+++ src/movies/actions.js
@@ -1,6 +1,8 @@
 import API_ROOT from '../api-config';
 
 export const GET_MOVIES = 'GET_MOVIES';
+export const GET_MOVIE = 'GET_MOVIE';
+export const RESET_MOVIE = 'RESET_MOVIE';
 
 export function getMovies() {
   return async function(dispatch) {
@@ -11,4 +13,21 @@ export function getMovies() {
       data: movies.results,
     });
   };
+}
+
+export function getMovie(id) {
+  return async function(dispatch) {
+    const response = await fetch(`${API_ROOT}/movie/${id}`);
+    const movie = await response.json();
+    return dispatch({
+      type: 'GET_MOVIE',
+      data: movie,
+    });
+  }
+}
+
+export function resetMovie() {
+  return {
+    type: 'RESET_MOVIE',
+  }
 }
